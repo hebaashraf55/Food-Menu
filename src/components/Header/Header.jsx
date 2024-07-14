61db48e229b919531f2dbfaa90ca00367e96faba
heba <hebaashraf81@gmail.com>
2024-07-14 19:36:53 +0300

    first-commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+           <h2>Order Your Favourit Food Here</h2>
+           <p> 
+            Choose Form a deverse meue featuring a delectable arry of dishes crafted with the finest ingredients and culinary expirtise . 
+            </p>
+           <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
